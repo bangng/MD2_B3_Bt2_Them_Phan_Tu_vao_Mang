e58eabe2a7ddd1e252bfba10bd07dcf74f1ef067
bangng <montersb@gmail.com>
2022-07-27 14:32:28 +0700

    mang

--- src/Main.java
+++ src/Main.java
@@ -2,24 +2,28 @@ import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
-        int[] array = {4,2,4,4,2,6,8,6,8,8};
+      Scanner scanner = new Scanner(System.in);
+      int [] array = {4,2,5,7,4,6};
         System.out.println("Enter a value");
         int x = scanner.nextInt();
         System.out.println("Enter index");
         int i = scanner.nextInt();
-        if (i <= -1 || i >= array.length - 1){
-            System.out.println("Can not insert!!!");
-            return;
+        if (i <=-1 || i >= array.length - 1){
+            System.out.println("Can not insert");
+//            return;
         }
         System.out.printf("%-5s","Old array: ");
-        for ( int value : array){
+        for (int value: array
+             ) {
             System.out.printf("%-3d",value);
+
         }
-        System.out.printf("%n%-5s","New array: ");
-        System.arraycopy(array, i, array, i+1, array.length - 1 - i);
-        array[i]= x;
-        for (int value: array) {
+        System.out.println();
+        System.out.printf("%-5s", "New array: ");
+//        System.arraycopy(array, i, array, );
+        array[i] = x;
+        for (int value: array
+             ) {
             System.out.printf("%-3d",value);
 
         }
